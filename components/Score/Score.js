2e44a4862d91eaa2f1677e2e8c62a4bd59d97f58
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-17 14:30:16 -0700

    score component

---
+++ components/Score/Score.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Score.css";
+
+const Score = props => (
+    <div className="container">
+        <p>
+            <span>Male: {props.male}</span>
+            <span>|</span>
+            <span>Female: {props.female}</span>
+        </p>
+        <p style={props.text} className='message'>{props.message}</p>
+    </div>
+);
+
+export default Score;
