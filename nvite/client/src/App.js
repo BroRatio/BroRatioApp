7abb4f6bda5df5b5116acfbaa36fba1cc7cea1fc
Chatura Ahangama <chaturadroid@gmail.com>
2018-07-20 13:13:52 -0700

    update with the routes

--- nvite/client/src/App.js
+++ nvite/client/src/App.js
@@ -2,19 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Score from './components/Score/Score'
 import "./App.css";
-import Landing from "./components/Landing/Landing";
+import Landing from "./pages/Landing/Landing";
 //import NoMatch from "./components/SimpleModal";
 // import LoginForm from './components/LoginForm/LoginForm'
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App loader">
-//         <Landing />
-//       </div>
-//     );
-//   }
-// }
 
 const App = () => {
   return <Router>
