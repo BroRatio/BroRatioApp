f788735a92b069692c69d1c3f482bb7e730342b4
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-25 10:59:42 -0700

    theme setup

--- nvite/client/src/App.js
+++ nvite/client/src/App.js
@@ -2,65 +2,80 @@ import React from "react";
 import { Redirect } from "react-router";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+// Material-UI imports
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { deepOrange, orange } from "@material-ui/core/colors";
 // Page Imports
 import Landing from "./pages/Landing/Landing";
 import Uploader from "./pages/Uploader/dropzone";
 import Start from "./pages/Start/Start";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: orange[500] },
+    secondary: { main: deepOrange[500] }
+  }
+});
+
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={props => {
-              let item;
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <div>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => {
+                let item;
 
-              if (localStorage.getItem("broLogin") == null) {
-                item = false;
-              } else {
-                item = JSON.parse(localStorage.getItem("broLogin")).loginStatus;
-              }
+                if (localStorage.getItem("broLogin") == null) {
+                  item = false;
+                } else {
+                  item = JSON.parse(localStorage.getItem("broLogin"))
+                    .loginStatus;
+                }
 
-              if (item === true) return <Redirect to="/landing" />;
-              else return <Start />;
-            }}
-          />
-          <Route
-            exact
-            path="/landing"
-            render={props => {
-              let item;
-              if (localStorage.getItem("broLogin") == null) {
-                item = false;
-              } else {
-                item = JSON.parse(localStorage.getItem("broLogin")).loginStatus;
-              }
+                if (item === true) return <Redirect to="/landing" />;
+                else return <Start />;
+              }}
+            />
+            <Route
+              exact
+              path="/landing"
+              render={props => {
+                let item;
+                if (localStorage.getItem("broLogin") == null) {
+                  item = false;
+                } else {
+                  item = JSON.parse(localStorage.getItem("broLogin"))
+                    .loginStatus;
+                }
 
-              if (item === false) return <Redirect to="/" />;
-              else return <Landing />;
-            }}
-          />
-          <Route
-            exact
-            path="/signup"
-            render={props => {
-              let item;
+                if (item === false) return <Redirect to="/" />;
+                else return <Landing />;
+              }}
+            />
+            <Route
+              exact
+              path="/signup"
+              render={props => {
+                let item;
 
-              if (localStorage.getItem("broLogin") == null) {
-                item = false;
-              } else {
-                item = JSON.parse(localStorage.getItem("broLogin")).loginStatus;
-              }
-              if (item === true) return <Redirect to="/landing" />;
-              else return <Uploader />;
-            }}
-          />
-        </Switch>
-      </div>
-    </Router>
+                if (localStorage.getItem("broLogin") == null) {
+                  item = false;
+                } else {
+                  item = JSON.parse(localStorage.getItem("broLogin"))
+                    .loginStatus;
+                }
+                if (item === true) return <Redirect to="/landing" />;
+                else return <Uploader />;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </MuiThemeProvider>
   );
 };
 
